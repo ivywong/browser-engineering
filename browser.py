21a95e06e25f20b7bfd2e1e7e203aaf2ea9db1d8
Ivy Wong <ivy@ivywong.dev>
2024-07-19 16:38:06 -0700

    handle ports and https

--- browser.py
+++ browser.py
@@ -5,13 +5,22 @@ import ssl
 class URL:
     def __init__(self, url: str) -> None:
         self.scheme, url = url.split("://", 1)
-        assert self.scheme == "http"
+        assert self.scheme in ["http", "https"]
 
         if "/" not in url:
             url = url + "/"
 
         self.host, url = url.split("/", 1)
         self.path = "/" + url
+
+        if self.scheme == "http":
+            self.port = 80
+        elif self.scheme == "https":
+            self.port = 443
+
+        if ":" in self.host:
+            self.host, port = self.host.split(":", 1)
+            self.port = int(port)
 
     def request(self):
         s = socket.socket(
@@ -20,7 +29,11 @@ class URL:
             proto=socket.IPPROTO_TCP,
         )
 
-        s.connect((self.host, 80))
+        s.connect((self.host, self.port))
+
+        if self.scheme == "https":
+            ctx = ssl.create_default_context()
+            s = ctx.wrap_socket(s, server_hostname=self.host)
 
         request = f"GET {self.path} HTTP/1.0\r\n"
         request += f"Host: {self.host}\r\n"
